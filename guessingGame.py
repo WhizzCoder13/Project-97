040fc033f7ef7b58435af4b4b84456f54a6e3bed
WhizzCoder13 <83590696+WhizzCoder13@users.noreply.github.com>
2022-04-18 12:54:32 +0100

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,9 @@
+num = 7
+chances = 5
+while chances < 5: 
+if guess == num :
+print ("CONGRATULATIONS YOU WON!!!")  
+break
+print("Guess a number between 1 and 9")
+if not chances < 5:
+print("YOU LOSE", the number is , num)    
